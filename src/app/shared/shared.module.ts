9d6d6680884191e7f44c90a5a310ca95ed747818
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-04-28 15:42:58 +0200

    add component

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StringConvertPipe } from './string-convert.pipe';
+import { SpliceArrayPipe } from './splice-array.pipe';
+import { StarComponent } from './star/star.component';
+
+
+
+@NgModule({
+  declarations: [
+    StringConvertPipe,
+    SpliceArrayPipe,
+    StarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
